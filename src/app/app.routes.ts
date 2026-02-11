70a7f283beb0274a552a46d056ec48f4c7bf1d5d
kiran-yendale <developer.yks@gmail.com>
2026-02-11 18:34:56 +0530

    Initial Angular digital business cards project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TemplatesComponent } from './pages/templates/templates.component';
+import { FeaturesComponent } from './pages/features/features.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'templates', component: TemplatesComponent }
+];
